305b1ce86b1a1c4f17004445f25ba0e75911e7e5
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-09-07 11:02:26 -0400

    added functionality and toggles for list navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,74 @@
 const deleteBtn = document.querySelectorAll('.del')
-const todoItem = document.querySelectorAll('span.not')
-const todoComplete = document.querySelectorAll('span.completed')
+const attractionItem = document.querySelectorAll('span.not')
+const attractionComplete = document.querySelectorAll('span.completed')
+const listTab = document.querySelectorAll('li.btn')
+const h1 = document.querySelector('h1')
+const lists = document.querySelectorAll('ul.list')
+
+Array.from(listTab).forEach((el)=>{
+    el.addEventListener('click', changeTabs)
+})
+function changeTabs(){
+  
+  Array.from(listTab).forEach(x => {
+    if (x.innerText != this.innerText){
+      x.style.display = 'inline';
+      // console.log(x.innerText);
+    }
+  })
+    if (this.innerText === 'My List'){
+      document.querySelector('ul.customL').style.display = 'flex';
+      Array.from(lists).forEach(x => {
+        if(x.className !== 'customL list'){ x.style.display = 'none'}
+      })
+    } else if (this.innerText === 'Gambling'){
+      document.querySelector('ul.gamblingL').style.display = 'flex';
+      Array.from(lists).forEach(x => {
+        if(x.className !== 'gamblingL list'){ x.style.display = 'none'}
+      })
+    } else if (this.innerText === 'Entertainment'){
+      document.querySelector('ul.entertainL').style.display = 'flex';
+      Array.from(lists).forEach(x => {
+        if(x.className !== 'entertainL list'){ x.style.display = 'none'}
+      })
+    } else if (this.innerText === 'Family Friendly'){
+      document.querySelector('ul.familyL').style.display = 'flex';
+      Array.from(lists).forEach(x => {
+        if(x.className !== 'familyL list'){ x.style.display = 'none'}
+      })
+    } else if (this.innerText === 'Food & Drink'){
+      document.querySelector('ul.diningL').style.display = 'flex';
+      Array.from(lists).forEach(x => {
+        if(x.className !== 'diningL list'){ x.style.display = 'none'}
+      })
+    } else {
+      console.log('no dice');
+    }
+  // console.log(this.innerText,Array.from(lists));
+  this.style.display = 'none';
+  h1.innerText = this.innerText;
+}
 
 Array.from(deleteBtn).forEach((el)=>{
-    el.addEventListener('click', deleteTodo)
+    el.addEventListener('click', deleteAttraction)
 })
 
-Array.from(todoItem).forEach((el)=>{
+Array.from(attractionItem).forEach((el)=>{
     el.addEventListener('click', markComplete)
 })
 
-Array.from(todoComplete).forEach((el)=>{
+Array.from(attractionComplete).forEach((el)=>{
     el.addEventListener('click', markIncomplete)
 })
 
-async function deleteTodo(){
-    const todoId = this.parentNode.dataset.id
+async function deleteAttraction(){
+    const attractionId = this.parentNode.dataset.id
     try{
-        const response = await fetch('todos/deleteTodo', {
+        const response = await fetch('attractions/deleteAttraction', {
             method: 'delete',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+                'attractionIdFromJSFile': attractionId
             })
         })
         const data = await response.json()
@@ -33,13 +80,13 @@ async function deleteTodo(){
 }
 
 async function markComplete(){
-    const todoId = this.parentNode.dataset.id
+    const attractionId = this.parentNode.dataset.id
     try{
-        const response = await fetch('todos/markComplete', {
+        const response = await fetch('attractions/markComplete', {
             method: 'put',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+                'attractionIdFromJSFile': attractionId
             })
         })
         const data = await response.json()
@@ -51,13 +98,13 @@ async function markComplete(){
 }
 
 async function markIncomplete(){
-    const todoId = this.parentNode.dataset.id
+    const attractionId = this.parentNode.dataset.id
     try{
-        const response = await fetch('todos/markIncomplete', {
+        const response = await fetch('attractions/markIncomplete', {
             method: 'put',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+                'attractionIdFromJSFile': attractionId
             })
         })
         const data = await response.json()
