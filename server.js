2e7d21905603d0a44e22740bab786bfddd96ea65
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-10-26 10:50:03 -0400

    deploy db fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
-      store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
   )
 
